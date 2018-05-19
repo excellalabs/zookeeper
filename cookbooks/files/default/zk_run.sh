ea1b5fd3580431d394fac69414d3ffb57590905b
Ali Jafari <ali.jafari@excella.com>
2018-05-19 19:31:21 -0400

    attach before zk_run

--- cookbooks/files/default/zk_run.sh
+++ cookbooks/files/default/zk_run.sh
@@ -4,12 +4,12 @@
 echo "zk_run run by $(whoami)"
 echo 'ruby /usr/local/bin/eni_switcher.rb' | sudo tee --append /usr/local/bin/run_eni_switch.sh
 bash /usr/local/bin/run_eni_switch.sh
+sleep 20
 # eni_name.sh is created by eni_switcher.rb
 source /usr/local/bin/eni_name.sh
-sleep 10
+echo "eni_name: ${eni_name}"
+/usr/local/bin/attach_ebs.py $eni_name /dev/xvdg /var/lib/zookeeper;
+sleep 20
 ruby /usr/local/bin/zk_server.rb
 sleep 10
-echo "eni_name: ${eni_name}"
-# /usr/local/bin/attach_ebs.py $eni_name /dev/xvdg /var/lib/zookeeper;
-
 service zookeeper restart
