266daa414a79e05d6e0635141d0d722e07023cb8
Ali Jafari <ali.jafari@excella.com>
2018-05-22 17:46:21 -0400

    debugging

--- cookbooks/zookeeper-config/recipes/default.rb
+++ cookbooks/zookeeper-config/recipes/default.rb
@@ -119,18 +119,25 @@ file '/etc/profile.d/keystore.sh' do
 end
 
 # prometheus setup
-cookbook_file '/opt/prometheus.yml' do
-  source 'prometheus.yml'
-  owner 'root'
-  group 'root'
-  mode '0755'
-end
+# cookbook_file '/opt/prometheus.yml' do
+#   source 'prometheus.yml'
+#   owner 'root'
+#   group 'root'
+#   mode '0755'
+# end
 
-bash 'Download prometheus jar script' do
-  code <<-SCRIPT
-    /usr/bin/aws s3api get-object --bucket ex-data-lab-binaries \
-      --key jmx_prometheus_javaagent-0.6.jar /opt/jmx_prometheus_javaagent-0.6.jar
-    SCRIPT
-  not_if { ::File.exist?('/opt/jmx_prometheus_javaagent-0.6.jar') }
-  not_if { node['test_kitchen'] }
-end
+# prometheus_agent = 'https://repo1.maven.org/maven2/io/prometheus/jmx/' \
+#                    'jmx_prometheus_javaagent/0.6/' \
+#                    'jmx_prometheus_javaagent-0.6.jar'
+# remote_file '/opt/jmx_prometheus_javaagent-0.6.jar' do
+#   source prometheus_agent
+# end
+
+# bash 'Download prometheus jar script' do
+#   code <<-SCRIPT
+#     /usr/bin/aws s3api get-object --bucket ex-data-lab-binaries \
+#       --key jmx_prometheus_javaagent-0.6.jar /opt/jmx_prometheus_javaagent-0.6.jar
+#     SCRIPT
+#   not_if { ::File.exist?('/opt/jmx_prometheus_javaagent-0.6.jar') }
+#   not_if { node['test_kitchen'] }
+# end
